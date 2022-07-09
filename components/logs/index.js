50d55fb26c0242463cf48b7d43721dbeab29fad3
seif <baazouziseif9@gmail.com>
2022-07-09 09:32:57 +0100

    change the design of the logs page

--- components/logs/index.js
+++ components/logs/index.js
@@ -1,13 +1,10 @@
 import { useState } from "react"
 
-import TrashIcon from "@heroicons/react/outline/TrashIcon"
-import PencilIcon from "@heroicons/react/outline/PencilIcon"
-
 import AddPopup from "./add"
 import EditPopup from "./edit"
 import DeletePopup from "./delete"
 
-import styles from "@/styles/Collections.module.css"
+import styles from "@/styles/Logs.module.css"
 
 function Logs({ logs, setLogs, collectionID }) {
   const [logID, setLogID] = useState(0)
@@ -32,24 +29,39 @@ function Logs({ logs, setLogs, collectionID }) {
 
   return (
     <>
-      <div className={styles.collectionsContainer}>
+      <div className={styles.container}>
         <div className={styles.listHeader}>
           <h4>logs List</h4>
           <button onClick={() => setShowAddPopup(true)}>Add new log</button>
         </div>
-        <ul className={styles.logsList}>
-          {logs.map(log => {
-            return (
-              <li key={log.logID} className={styles.collectionItem}>
-                <p>{`${log.label} => ${log.value}`}</p>
-                <div className={styles.icons}>
-                  <PencilIcon onClick={() => showEditPopupHandler(log)} />
-                  <TrashIcon onClick={() => showDeletePopupHandler(log)} />
-                </div>
-              </li>
-            )
-          })}
-        </ul>
+        <div className={styles.table}>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Label</th>
+                <th>Value</th>
+                <th>Date</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {logs.map(log => {
+                return (
+                  <tr key={log.logID}>
+                    <td>{log.logID}</td>
+                    <td>{log.label}</td>
+                    <td>{log.value}</td>
+                    <td>{new Date(log.logDate).toDateString()}</td>
+                    <td><p className={`${styles.btn} ${styles.edit}`} onClick={() => showEditPopupHandler(log)}>edit</p></td>
+                    <td><p className={`${styles.btn} ${styles.delete}`} onClick={() => showDeletePopupHandler(log)}>delete</p></td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
         
       {showAddPopup &&
